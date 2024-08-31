5a9babb1c3cd4a5c43bbfc9679470ef5574a0b91
Rafael Germann <rafael.germann@gmail.com>
2024-08-31 00:23:32 -0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using Dummy;
+using Grpc.Core;
+
+namespace grpc_csharp.client;
+public class Program
+{
+    const string target = "127.0.0.1:50051";
+    private static void Main(string[] args)
+    {
+        var channel = new Channel(target, ChannelCredentials.Insecure);
+
+        channel.ConnectAsync().ContinueWith((task) =>
+        {
+            if (task.Status == TaskStatus.RanToCompletion)
+                Console.WriteLine("Conectado com sucesso!");
+        });
+        var client = new DummyService.DummyServiceClient(channel);
+        channel.ShutdownAsync().Wait();
+        Console.ReadKey();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+
+namespace grpc_csharp.server;
+internal class Program
+{
+    const int Port = 50051;
+
+    private static void Main(string[] args)
+    {
+        Server server = null;
+        try
+        {
+            server = new Server()
+            {
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            Console.WriteLine("Servidor escutando na porta : " + Port);
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao iniciar o servidor : " + ex.Message);
+            throw;
+        }
+        finally
+        {
+            if (server != null)
+                server.ShutdownAsync().Wait();
+        }
+    }
+}
